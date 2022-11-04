bd4ac651f5315413b23b31b62eec0b8b8563fb16
wuyushuwys <wuyushuwys@outlook.com>
2022-11-04 16:52:39 -0400

    remove not used

--- models/__init__.py
+++ models/__init__.py
@@ -1,19 +1,3 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
-
-import argparse
-import models.discriminator
-
-from .model import Model
-from .discriminator import UNetDiscriminatorSN
-
-
-def create_discrinimator(args:argparse.Namespace):
-
-    d_arch = getattr(args, 'd_arch')
-    discriminator_module = models.discriminator
-    assert hasattr(discriminator_module, d_arch), f"{d_arch} not implemented or not found"
-    d_model = getattr(discriminator_module, d_arch)(args)
-
-    return d_model
